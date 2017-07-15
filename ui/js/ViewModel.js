5a0ca4050fa6eaae3c67201b919b1e71df8f06fc
Roland Tapken <rt@tasmiro.de>
2017-07-15 23:01:54 +0200

    Added entry id to widget element

--- ui/js/ViewModel.js
+++ ui/js/ViewModel.js
@@ -9,50 +9,14 @@
 (function() {
 	"use strict";
 
-	define(['knockout', 'ko/mapper', 'ko/translate', 'RemoteData', 'locales', 'moment', 'utils'], 
-		function(ko, kom, t, RemoteData, locales, moment, utils) {
+	define(['knockout', 'RemoteData', 'moment', 'utils', 'app/Locale', 'app/Entries'], 
+		function(ko, RemoteData, moment, utils, Locale, Entries) {
 			var exports = { };
 
 			/**
 			 * Exports the observables 'locale' and 'locale.available'
 			 **/
-			exports.locale = (function initializeLocales() {
-				// Load available locales
-				var available = [];
-				for (var id in locales) {
-					if (locales.hasOwnProperty(id)) {
-						available.push({ id: id, label: locales[id] });
-					}
-				}
-
-				// Initialize current locale
-				var defLang = (navigator.language || navigator.userLanguage || 'en').toLowerCase();
-				if (defLang.indexOf('-') > 0) {
-					defLang = defLang.substring(0, defLang.indexOf('-'));
-				}
-				var locale = ko.observable(defLang);
-				
-				// Only use the current locale when it's in set the of available locales.
-				// Otherwise, fallback to 'en' as default locale.
-				var result = ko.pureComputed({
-					read: function() {
-						var currentValue = locale();
-						if (currentValue && locales[currentValue] !== undefined) {
-							return currentValue;
-						}
-						return 'en';
-					},
-					write: locale
-				});
-				result.available = available;
-
-				// Update knockout.translate when locale changes
-				new RemoteData(ko.pureComputed(function() {
-					return 'js/locale/' + result() + '.json';
-				})).done(t.map).load();
-
-				return result;
-			})();
+			exports.locale = new Locale(exports);
 
 			/**
 			 * Exports the current time as locale dependent string.
@@ -71,18 +35,14 @@
 
 			// Load initial set of entries...
 			// TODO This should be filled by the python backend
-			exports.entries = new RemoteData('test/data.json').done(function(data) {
-				var result = [];
-				for (var id in data) {
-					if (data.hasOwnProperty(id)) {
-						data[id].id = id;
-						result.push(data[id]);
-					}
-				}
-				return result;
-			});
+			exports.entries = new Entries(exports);
+
+			// Create a shortcut
+			exports.selected = exports.entries.selected;
+
 
 			// Selected entry
+			// TODO Move this into app/Entries
 			exports.selected = (function() {
 				var selected = ko.observable();
 
