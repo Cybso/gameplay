89be37456a6d803c178f65fc447f13d312e06c27
Roland Tapken <rt@tasmiro.de>
2017-07-25 08:48:53 +0200

    Use freedesktop main categories for SystemAppProvider

--- gameplay/providers/SystemAppProvider.py
+++ gameplay/providers/SystemAppProvider.py
@@ -1,6 +1,23 @@
 ###
 # Provides access to the system apps (global .desktop files for Linux,
-# Start Menu on windows.
+# Start Menu on windows).
+#
+# Linux:
+#  SystemAppProvider parses and interprets .desktop files
+#  from all paths defined by QStandardPaths::ApplicationsLocation
+#  (http://doc.qt.io/qt-5/qstandardpaths.html).
+#
+#  The processing is done by DesktopEntryProvider (which primary
+#  is used for CUSTOM .desktop files).
+#
+#  Only the Main Categories defined by 
+#  https://standards.freedesktop.org/menu-spec/latest/apa.html
+#  will be returned. Other categories will be ignored.
+#  This list can be overwritten in the configuration file:
+#
+#    [SystemAppProvider]
+#    categories=Game Video Network
+#
 # FIXME Implement Windows
 # FIXME Implement OSX
 ###
@@ -13,15 +30,39 @@ if system == 'Linux':
 	from .DesktopEntryProvider import DesktopEntryProvider
 	from PyQt5.QtCore import QDir, QStandardPaths
 
+	# https://standards.freedesktop.org/menu-spec/latest/apa.html
+	MAIN_CATEGORIES = [
+		"AudioVideo",
+		"Development",
+		"Education",
+		"Game",
+		"Graphics",
+		"Network",
+		"Office",
+		"Science",
+		"Settings",
+		"System",
+		"Utility"
+	];
+
 	class SystemAppProvider(DesktopEntryProvider):
 		def __init__(self, settings):
 			DesktopEntryProvider.__init__(self, settings, [QDir.toNativeSeparators(x) for x in QStandardPaths.standardLocations(QStandardPaths.ApplicationsLocation)])
 
+		###
+		# Lists are passed by reference, so this method
+		# can be used to remove all Non-Standard-Categories
+		# from the list.
+		###
 		def filter_category(self, categories):
-			for c in ['WebBrowser', 'Game', 'TV']:
-				if c in categories:
-					return True
-			return False
+			keep_categories = self.settings.getlist('SystemAppProvider', 'categories', fallback=MAIN_CATEGORIES)
+			# Work on a copy, because we are going to modify
+			# the list while iterating it.
+			for c in  categories[:]:
+				if not c in keep_categories:
+					categories.remove(c)
+					print(categories)
+			return len(categories) > 0
 
 #elif system == 'Darwin':
 #elif system == 'Windows':
